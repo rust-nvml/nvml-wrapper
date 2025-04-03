9fb9526ddc76b92d883e71330198b612adff3ef8
David Carlier <devnexen@gmail.com>
2025-04-03 21:02:39 +0100

    adding device::virtualization_mode() wrapper.
    Returns whether the current GPU device is within a virtualization mode
    or bare metal.

--- nvml-wrapper/src/test_utils.rs
+++ nvml-wrapper/src/test_utils.rs
@@ -105,6 +105,7 @@ impl ShouldPrint for PowerSource {}
 impl ShouldPrint for DeviceArchitecture {}
 impl ShouldPrint for PcieLinkMaxSpeed {}
 impl ShouldPrint for DeviceAttributes {}
+impl ShouldPrint for GpuVirtualizationMode {}
 
 #[cfg(target_os = "windows")]
 impl ShouldPrint for DriverModelState {}
